5cc03551135a2720200f822c0666c119db1300ee
Oscar Otero <oom@oscarotero.com>
2021-08-30 20:14:29 +0200

    don't load reactDOM in Deno

--- app/deps.ts
+++ app/deps.ts
@@ -1,4 +1,3 @@
-export { default as ReactDOM } from "https://deno.land/x/react_deno@17.0.2/dom.ts";
 export { default as React } from "https://deno.land/x/react_deno@17.0.2/react.ts";
 export { default as classNames } from "https://cdn.jsdelivr.net/gh/JedWatson/classnames@next/index.js";
 export type { KeyboardEvent } from "https://deno.land/x/react_deno@17.0.2/src/react.d.ts";
--- app/main.jsx
+++ app/main.jsx
@@ -1,4 +1,5 @@
-import { React, ReactDOM } from "./deps.ts";
+import { React } from "./deps.ts";
+import ReactDOM from "https://deno.land/x/react_deno@17.0.2/dom.ts";
 import App from "./components/app.tsx";
 
 function render() {
